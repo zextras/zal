a8d26a09fab93a4dea55c42873d8678ed5ad688e
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2024-12-10 14:37:34 +0100

    feat: added pagination attributes to SearchUsersByFeatureResponse

--- src/main/java/org/openzal/zal/soap/SearchUsersByFeatureResponse.java
+++ src/main/java/org/openzal/zal/soap/SearchUsersByFeatureResponse.java
@@ -11,13 +11,15 @@ import java.util.List;
 
 public class SearchUsersByFeatureResponse {
 
-  public static SoapResponse buildResponse(ZimbraContext zimbraContext, List<Entry> entries) {
+  public static SoapResponse buildResponse(ZimbraContext zimbraContext, List<Entry> entries, int total, boolean more) {
     var zsc = ((ZimbraContextImpl)zimbraContext).getZimbraSoapContext();
     var response = zsc.createElement(AccountConstants.SEARCH_USERS_BY_FEATURE_RESPONSE);
     var attributes = new HashSet<String>();
     attributes.add(ZAttrProvisioning.A_mail);
     attributes.add(ZAttrProvisioning.A_uid);
     attributes.add(ZAttrProvisioning.A_displayName);
+    response.addAttribute(AccountConstants.A_TOTAL, total);
+    response.addAttribute(AccountConstants.A_MORE, more);
     entries.forEach(a -> ToXML.encodeAccount(response, (Account) a.toZimbraEntry(), true, attributes, null));
     return new SoapResponseImpl(response, null);
   }
