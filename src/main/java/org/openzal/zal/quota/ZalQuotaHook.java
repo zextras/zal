765a2175fd45df157ed7071d859625190f7def4b
drazen04 <hangtime23@hotmail.it>
2026-03-30 12:25:59 +0200

    fix: remove vavr dependency + use custom result

--- src/main/java/org/openzal/zal/quota/ZalQuotaHook.java
+++ src/main/java/org/openzal/zal/quota/ZalQuotaHook.java
@@ -4,20 +4,20 @@ import com.zextras.mailbox.quota.IsOverQuota;
 import com.zextras.mailbox.quota.QuotaHook;
 import com.zextras.mailbox.quota.QuotaHookSingleton;
 import com.zimbra.cs.account.Account;
-import io.vavr.Function2;
-import io.vavr.control.Try;
+
 import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class ZalQuotaHook implements QuotaHook {
 
 	private final Function<org.openzal.zal.Account, Boolean> overQuotaFunction;
-	private final Function2<org.openzal.zal.Account, Long, Try<Void>> addMessageFunction;
+	private final BiFunction<org.openzal.zal.Account, Long, Result> addMessageFunction;
 	private final BiConsumer<org.openzal.zal.Account, Long> deleteMessageFunction;
 
 	public static synchronized void setInstance(
 			Function<org.openzal.zal.Account, Boolean> overQuotaFunction,
-			Function2<org.openzal.zal.Account, Long, Try<Void>> addMessageFunction,
+			BiFunction<org.openzal.zal.Account, Long, Result> addMessageFunction,
 			BiConsumer<org.openzal.zal.Account, Long> deleteMessageFunction
 	) {
 		var hook = new ZalQuotaHook(overQuotaFunction, addMessageFunction, deleteMessageFunction);
@@ -25,7 +25,7 @@ public class ZalQuotaHook implements QuotaHook {
 	}
 
 	private ZalQuotaHook(Function<org.openzal.zal.Account, Boolean> overQuotaFunction,
-			Function2<org.openzal.zal.Account, Long, Try<Void>> addMessageFunction,
+			BiFunction<org.openzal.zal.Account, Long, Result> addMessageFunction,
 			BiConsumer<org.openzal.zal.Account, Long> deleteMessageFunction) {
 		this.overQuotaFunction = overQuotaFunction;
 		this.addMessageFunction = addMessageFunction;
@@ -42,8 +42,8 @@ public class ZalQuotaHook implements QuotaHook {
 	@Override
 	public IsOverQuota addMessage(Account acct, long newTotalMailboxUsage) {
 		final org.openzal.zal.Account zalAccount = new org.openzal.zal.Account(acct);
-		final Try<Void> apply = addMessageFunction.apply(zalAccount).apply(newTotalMailboxUsage);
-		if (apply.isFailure()) {
+		final Result result = addMessageFunction.apply(zalAccount, newTotalMailboxUsage);
+		if (!result.isSuccess()) {
 			return new IsOverQuota(true);
 		}
 		return new IsOverQuota(false);
@@ -54,4 +54,6 @@ public class ZalQuotaHook implements QuotaHook {
 		final org.openzal.zal.Account zalAccount = new org.openzal.zal.Account(acct);
 		deleteMessageFunction.accept(zalAccount, size);
 	}
+
+	public record Result(boolean isSuccess) {}
 }
