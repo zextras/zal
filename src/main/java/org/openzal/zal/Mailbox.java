06a658e049654756988cf1e314b0ce75ca5df44a
drazen04 <hangtime23@hotmail.it>
2026-03-30 10:50:55 +0200

    chore: rename method changed in mailbox

--- src/main/java/org/openzal/zal/Mailbox.java
+++ src/main/java/org/openzal/zal/Mailbox.java
@@ -2840,7 +2840,7 @@ public class Mailbox
   {
     try
     {
-      mMbox.checkSizeChangeAddOperation(newSize);
+      mMbox.checkSizeChangeOnAddOperation(newSize);
     }
     catch (ServiceException e)
     {
