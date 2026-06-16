97f6353e6ba15e5cdb13e21f9348b7453338e898
zextrasGiovanniDefacci <66628670+zextrasGiovanniDefacci@users.noreply.github.com>
2026-06-16 16:42:26 +0200

    fix: dbpool (#118)

--- src/test/java/org/openzal/zal/db/DbPool.java
+++ src/test/java/org/openzal/zal/db/DbPool.java
@@ -1,5 +1,20 @@
 package org.openzal.zal.db;
 
-public class DbPool extends com.zimbra.cs.db.DbPool {
+import com.zimbra.common.service.ServiceException;
+
+public class DbPool {
+
+  public static void startup() {
+    com.zimbra.cs.db.DbPool.global();
+  }
+
+  public static com.zimbra.cs.db.DbPool.DbConnection getConnection() throws ServiceException {
+    return com.zimbra.cs.db.DbPool.global().getConnectionInstance();
+  }
+
+  public static void shutdown() throws Exception {
+    com.zimbra.cs.db.DbPool.shutdown();
+  }
+
 
 }
