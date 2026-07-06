52144624e47a68ac1a24719c97ff75f7623f2312
Giovanni De Facci <giovanni.defacci@zextras.com>
2026-07-06 12:28:50 +0200

    feat: make MimeHandlerManager.HandlerInfo public, add MimeHandlerManager.setHandlers/getHandlers

--- src/main/java/org/openzal/zal/index/IndexerManager.java
+++ src/main/java/org/openzal/zal/index/IndexerManager.java
@@ -21,18 +21,12 @@
 package org.openzal.zal.index;
 
 import com.zimbra.cs.convert.AttachmentInfo;
-import com.zimbra.cs.convert.ConversionException;
 import com.zimbra.cs.mime.*;
 import org.apache.lucene.document.Document;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import org.openzal.zal.Utils;
-import org.openzal.zal.log.ZimbraLog;
 
 import javax.activation.DataSource;
-import java.io.IOException;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -41,19 +35,10 @@ import java.util.Set;
 
 public class IndexerManager
 {
-  private static final Field                     sMimeHandlerManagerHandlersField;
-  private static final Class<MimeHandlerManager> sMimeHandlerManager;
-
-  private static final Class<?>                  sHandlerInfo;
-  private static final Constructor<?>            sHandlerInfoConstructor;
-  private static final Field                     sHandlerInfoClazzField;
-  private static final Field                     sHandlerInfoContentTypeField;
-  private static final Field                     sHandlerInfoRealMimeTypeField;
-
-  private static final List<Indexer>             sIndexerList;
+  private static final List<Indexer>             sIndexerList = Collections.synchronizedList(new LinkedList<Indexer>());
 
   @Nullable
-  private static       Map<String, Object>       sOriginalMap;
+  private static       Map<String, MimeHandlerManager.HandlerInfo>       sOriginalMap;
 
 
   public void register(Indexer indexer)
@@ -66,71 +51,20 @@ public class IndexerManager
     sIndexerList.remove(indexer);
   }
 
-
-  static
-  {
-    try
-    {
-      sOriginalMap = null;
-      sIndexerList = Collections.synchronizedList(new LinkedList<Indexer>());
-
-      sMimeHandlerManager = com.zimbra.cs.mime.MimeHandlerManager.class;
-      sHandlerInfo = sMimeHandlerManager.getClassLoader().loadClass("com.zimbra.cs.mime.MimeHandlerManager$HandlerInfo");
-
-      sHandlerInfoConstructor = sHandlerInfo.getDeclaredConstructors()[0];
-      sHandlerInfoConstructor.setAccessible(true);
-
-      sMimeHandlerManagerHandlersField = sMimeHandlerManager.getDeclaredField("sHandlers");
-      sMimeHandlerManagerHandlersField.setAccessible(true);
-
-      sHandlerInfoClazzField = sHandlerInfo.getDeclaredField("clazz");
-      sHandlerInfoClazzField.setAccessible(true);
-
-      sHandlerInfoContentTypeField = sHandlerInfo.getDeclaredField("realMimeType");
-
-      sHandlerInfoContentTypeField.setAccessible(true);
-
-      sHandlerInfoRealMimeTypeField = sHandlerInfo.getDeclaredField("mimeType");
-      sHandlerInfoRealMimeTypeField.setAccessible(true);
-    }
-    catch (Throwable ex)
-    {
-      ZimbraLog.extensions.fatal("ZAL Reflection Initialization Exception: " + Utils.exceptionToString(ex));
-      throw new RuntimeException(ex);
-    }
-  }
-
   public void attachToZimbra()
   {
-    try
-    {
-      sOriginalMap = (Map<String,Object>) sMimeHandlerManagerHandlersField.get(null);
-      sMimeHandlerManagerHandlersField.set(
-              null,
-              new IndexerProxyMap(
-                      sOriginalMap,
-                      new MimeHandlerProviderImpl()
-              )
-      );
-    }
-    catch (IllegalAccessException e)
-    {
-      throw new RuntimeException(e);
-    }
+    sOriginalMap = MimeHandlerManager.getHandlers();
+    MimeHandlerManager.setHandlers(new IndexerProxyMap(
+            sOriginalMap,
+            new MimeHandlerProviderImpl()
+    ));
   }
 
   public void detach()
   {
-    try
+    if( sOriginalMap != null )
     {
-      if( sOriginalMap != null )
-      {
-        sMimeHandlerManagerHandlersField.set(null, sOriginalMap);
-      }
-    }
-    catch (IllegalAccessException e)
-    {
-      throw new RuntimeException(e);
+      MimeHandlerManager.setHandlers(sOriginalMap);
     }
   }
 
@@ -151,7 +85,7 @@ public class IndexerManager
   {
     @Nullable
     @Override
-    public Object getMimeHandlerFor(String contentType, String fileExtension)
+    public MimeHandlerManager.HandlerInfo getMimeHandlerFor(String contentType, String fileExtension)
     {
       Indexer indexer = getBestIndexer(contentType, fileExtension);
       if (indexer != null)
@@ -166,16 +100,16 @@ public class IndexerManager
     }
   }
 
-  private Object createHandlerInfoProxy(Class<?> cls, String contentType)
+  private MimeHandlerManager.HandlerInfo createHandlerInfoProxy(Class<? extends MimeHandler> cls, String contentType)
   {
     try
     {
-      Object info = sHandlerInfoConstructor.newInstance();
+      var info = new MimeHandlerManager.HandlerInfo();
 
-      sHandlerInfoContentTypeField.set(info, contentType);
-      sHandlerInfoClazzField.set(info, cls);
+      info.setRealMimeType(contentType);
+      info.setClass(cls);
 
-      sHandlerInfoRealMimeTypeField.set(info, new MimeTypeInfo() {
+      info.setMimeType(new MimeTypeInfo() {
         @Override
         public String[] getMimeTypes() {
           return new String[0];
@@ -230,9 +164,8 @@ public class IndexerManager
       return false;
     }
 
-    //this methods breaks binary compatibility for ZAL given two different parameters types
     @Override
-    protected void addFields(Document doc) throws MimeHandlerException
+    protected void addFields(Document doc)
     {
 
     }
@@ -280,7 +213,7 @@ public class IndexerManager
     }
 
     @Override
-    public String convert(AttachmentInfo doc, String urlPart) throws IOException, ConversionException
+    public String convert(AttachmentInfo doc, String urlPart)
     {
       throw new UnsupportedOperationException();
     }
